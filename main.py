5b0d7ff7e908aebb7517cab44218113aec346faf
Le0ks <TLe0ks@yandex.ru>
2023-11-24 18:14:21 +0500

    added second function

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ def my_superfunction():
     print('What an awesome print!')
 
 
+def second_function():
+    print(":)")
+
+
 def main():
     print('My first git program')
     print('And I change it every day')
     print('Again')
     print('UFO came and added this line')
     my_superfunction()
+    second_function()
 
 
 if __name__ == '__main__':
